355296f5b1beb15f5096fb787df26329674e28c4
Jayanth <76648322+jayanth353@users.noreply.github.com>
2022-02-26 11:07:08 +0530

    Update main.js

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -1,7 +1,7 @@
 
 var typed = new Typed('#type', {
 // Waits 1000ms after typing "First"
-strings: ["👋Hi there ! I'm Jayanth^1000",'^800','Welcome to my website!^1000'],
+strings: ["👋Hi there ! I'm Jayanth.^1000",'^800','Welcome to my website!^1000'],
 typeSpeed:70,
 loop:true,
 backSpeed:20
